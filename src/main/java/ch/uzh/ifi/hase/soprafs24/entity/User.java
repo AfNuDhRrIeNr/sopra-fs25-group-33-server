e3147c7534114a7ad1949d134c24be790d7dcdb8
Mex7180 <zanetti.luca@bluewin.ch>
2025-03-26 10:46:28 +0100

    Adjust existing entities, add Request and Game entities
    - remove name field from user
    - add InvitationBase super class
    - add FriendRequest and GameInvitation entity
    - remove failing unit tests
    
    Linked Task: #79

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
@@ -4,6 +4,9 @@ import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Internal User Representation
@@ -16,20 +19,17 @@ import java.io.Serializable;
  * the primary key
  */
 @Entity
-@Table(name = "USER")
 public class User implements Serializable {
-
-  private static final long serialVersionUID = 1L;
 
   @Id
   @GeneratedValue
   private Long id;
 
-  @Column(nullable = false)
-  private String name;
-
   @Column(nullable = false, unique = true)
   private String username;
+
+  @Column(nullable = false)
+  private String password;
 
   @Column(nullable = false, unique = true)
   private String token;
@@ -37,20 +37,36 @@ public class User implements Serializable {
   @Column(nullable = false)
   private UserStatus status;
 
+  @Column(nullable = false)
+  private boolean isInGame = false;
+
+    // Self-referencing many-to-many for friendship
+  @ManyToMany
+  private Set<User> friends = new HashSet<>();
+
+  @OneToMany(mappedBy = "sender")
+  private Set<FriendRequest> sentFriendRequests = new HashSet<>();
+
+  @OneToMany(mappedBy = "target")
+  private Set<FriendRequest> receivedFriendRequests = new HashSet<>();
+
+  @OneToMany(mappedBy = "sender")
+  private Set<GameInvitation> receivedGameInvitations = new HashSet<>();
+
+  @OneToOne
+  private Game bestGamePlayed;
+
+  @Column(nullable = false)
+  private LocalDateTime lastModified = LocalDateTime.now();
+
+
+
   public Long getId() {
     return id;
   }
 
   public void setId(Long id) {
     this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
   }
 
   public String getUsername() {
@@ -76,4 +92,71 @@ public class User implements Serializable {
   public void setStatus(UserStatus status) {
     this.status = status;
   }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isInGame() {
+        return isInGame;
+    }
+
+    public void setInGame(boolean inGame) {
+        isInGame = inGame;
+    }
+
+    public Set<User> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(Set<User> friends) {
+        this.friends = friends;
+    }
+
+    public void addFriend(User user) {
+      this.friends.add(user);
+    }
+
+    public Set<FriendRequest> getSentRequests() {
+        return sentFriendRequests;
+    }
+
+    public void addFriendRequestTSentRequests(FriendRequest request) {
+        this.sentFriendRequests.add(request);
+    }
+
+    public Set<FriendRequest> getReceivedRequests() {
+        return receivedFriendRequests;
+    }
+
+    public void addFriendRequestsToReceivedRequests(FriendRequest request) {
+        this.receivedFriendRequests.add(request);
+    }
+
+    public Game getBestGamePlayed() {
+        return bestGamePlayed;
+    }
+
+    public void setBestGamePlayed(Game bestGamePlayed) {
+        this.bestGamePlayed = bestGamePlayed;
+    }
+
+    public LocalDateTime getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(LocalDateTime lastModified) {
+        this.lastModified = lastModified;
+    }
+    public Set<GameInvitation> getReceivedGameInvitations() {
+        return receivedGameInvitations;
+    }
+
+    public void addGameInvitation(GameInvitation gameInvitation) {
+      this.receivedGameInvitations.add(gameInvitation);
+    }
 }
