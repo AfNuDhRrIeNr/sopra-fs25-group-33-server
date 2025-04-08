af8e26c962cf50322c35b07a5ecd5fc9806312af
LucKer58 <lucien.kern@bluewin.ch>
2025-04-08 12:00:26 +0200

    Implement functionality for validation endpoint for single user

--- src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/WebSocketController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/WebSocketController.java
@@ -3,6 +3,8 @@ package ch.uzh.ifi.hase.soprafs24.websocket;
 import ch.uzh.ifi.hase.soprafs24.constant.MessageStatus;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.MessageGameStateMessageDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.GameStateDTO;
+import ch.uzh.ifi.hase.soprafs24.service.MoveValidatorService;
+import java.util.List;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,49 +20,98 @@ import org.springframework.web.server.ResponseStatusException;
 public class WebSocketController {
 
     @Autowired
+    private MoveValidatorService moveValidatorService;
+
+    @Autowired
     SimpMessagingTemplate simpleMessagingTemplate;
 
     private static final Logger logger = LoggerFactory.getLogger(WebSocketController.class);
 
-
     // ------------------ Game State ---------------------------------------
     @MessageMapping("/game_states/{gameId}")
     @SendTo("/topic/game_states/{gameId}")
-    public void handleGameStates(@DestinationVariable String gameId, GameStateDTO gameState) {
+    public MessageGameStateMessageDTO handleGameStates(@DestinationVariable String gameId, GameStateDTO gameState) {
         logger.debug("[LOG] Game endpoint reached with gameId: '{}' and gameState entity: '{}'",gameId, gameState.toString());
         // Verify DTO
         try {
             gameState.isValid();
         } catch (IllegalArgumentException exception) {
-            simpleMessagingTemplate.convertAndSend("/topic/game_states/"+gameId, new MessageGameStateMessageDTO(
+            return new MessageGameStateMessageDTO(
                     Long.valueOf(gameId),
                     MessageStatus.ERROR,
                     exception.getMessage(),
                     null
-            ));
+            );
         }
         if(gameState.getId() != Long.valueOf(gameId)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN,"The game id of the object and destination are not equal!");
         }
 
-        // verify words of game
+        // Check if it's a validation request
+        if (gameState.getAction().equals("VALIDATE")) {
+            try {
+                // Validate the move
+                List<String> formedWords = moveValidatorService.validateMoveAndExtractWords(
+                        Long.valueOf(gameId), 
+                        gameState.getBoard()
+                );
+                
+                // Set the validated words in the response
+                gameState.setValidatedWords(formedWords);
+                
+                // Send validation success response ONLY to the requesting user
+                simpleMessagingTemplate.convertAndSend(
+                        "/topic/game_states/users/" + gameState.getPlayerId(), 
+                        new MessageGameStateMessageDTO(
+                            Long.valueOf(gameId),
+                            MessageStatus.VALIDATION_SUCCESS,
+                            "Move validation successful",
+                            gameState
+                        )
+                );
+                
+                logger.debug("[LOG] Move validation successful for gameId: '{}', formed words: '{}'", gameId, formedWords);
+                
+                // Return null since we've already sent the message
+                return null;
+            } 
+            catch (ResponseStatusException e) {
+                // Send validation error ONLY to the requesting user
+                simpleMessagingTemplate.convertAndSend(
+                        "/topic/game_states/users/" + gameState.getPlayerId(), 
+                        new MessageGameStateMessageDTO(
+                            Long.valueOf(gameId),
+                            MessageStatus.VALIDATION_ERROR,
+                            e.getReason(),
+                            gameState
+                        )
+                );
+                
+                logger.debug("[LOG] Move validation failed for gameId: '{}', reason: '{}'", gameId, e.getReason());
+                
+                // Return null since we've already sent the message
+                return null;
+            }
+        }
+        else if (gameState.getAction().equals("SUBMIT")) {
+            // For submit, just broadcast the received message as is
+            // (persistence will be added later)
+            return new MessageGameStateMessageDTO(
+                Long.valueOf(gameId),
+                MessageStatus.SUCCESS,
+                "Move submitted",
+                gameState
+            );
+        }
 
-        // Add new gameState no database (if submit)
-
-        // Send gameState back to users
-
-
-
-        simpleMessagingTemplate.convertAndSend("/topic/game_states/"+gameId, new MessageGameStateMessageDTO(
+        // Default response for other cases
+        return new MessageGameStateMessageDTO(
                 Long.valueOf(gameId),
                 MessageStatus.SUCCESS,
                 "GameState successfully received",
                 gameState
-        ));
+        );
     }
 
-
-
     // ------------------ Moves ---------------------------------------------
-
 }
